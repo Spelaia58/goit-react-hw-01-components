48caf160ece9c4f0d13768ee1af5c5e6a5039548
spelaia58 <spelaia58@gmail.com>
2020-07-15 18:05:44 +0300

    w1

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 import styles from "./Friends.module.css";
 import defaultImage from "./default.jpg";
 
-const Friend = ({ avatar, name, IsOnline }) => {
+const Friend = ({ avatar, name, isOnline }) => {
   return (
     <>
       <span
         className={styles.Status}
         style={{
-          backgroundColor: IsOnline ? "red" : "green",
+          backgroundColor: isOnline ? "red" : "green",
         }}
       ></span>
       <img className={styles.Avatar} src={avatar} alt={name} width="48" />
